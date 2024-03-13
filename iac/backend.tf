476a6779b75ea4b4d1c81306c48490f6ddb5bf9a
Dawid Borycki <dawid@borycki.com.pl>
2024-03-13 14:58:58 +0100

    Updates

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "rg-aks-people"
-    storage_account_name  = "peoplewebappstore"
-    container_name        = "terraform"
-    key                   = "prod.terraform.tfstate"
-    use_oidc              = true
+    resource_group_name   = var.resource_group_name
+    storage_account_name  = var.storage_account_name
+    container_name        = var.container_name
+    key                   = "prod.terraform.tfstate"    
   }
 }
---
+++ iac/terraform.tfvars
@@ -0,0 +1,3 @@
+STORAGE_ACCOUNT_NAME="rg-aks-people"
+STORAGE_ACCOUNT_NAME="peoplewebappstore"
+CONTAINER_NAME="terraform"
