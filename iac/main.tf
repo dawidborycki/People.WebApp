dd0001cf0c5dcc52f937d864a0a845f2945069c7
Dawid Borycki <dawid@borycki.com.pl>
2024-03-16 16:53:16 +0000

    AcrPull

--- iac/main.tf
+++ iac/main.tf
@@ -43,6 +43,13 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
   ]
 }
 
+resource "azurerm_role_assignment" "role_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.kubernetes_cluster.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.container_registry.id
+  skip_service_principal_aad_check = true
+}
+
 resource "azurerm_mssql_server" "sql_server" {
   name                         = "aks-sql-people-tf"
   resource_group_name          = azurerm_resource_group.rg-tf.name
